37a617b8a73d3166114d5dfd85674358e052f1c3
chandrashekar74 <chandrashekarh7409@gamil.com>
2026-02-10 15:24:41 +0530

    completed till user methods

---
+++ exercise.java
@@ -0,0 +1,25 @@
+import java.lang.System;
+class exercise{
+    public static void main(String[] args)
+    {
+        String name = "vidyavahini";
+        int age = 20;
+        float height = 5.5f;
+        double weight = 60.5;
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("Height: "+height);
+        System.out.println("Weight: "+weight);
+    }
+}
+
+class exercise2{
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+        System.out.println("The sum of a and b is"+(a+b));
+        System.out.println("The difference of a and b is"+(a-b));
+        System.out.println("The product of a and b is"+(a*b));
+        System.out.println("The quotient of a and b is"+(a/b));
+    }
+}
