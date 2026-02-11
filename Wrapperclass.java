0cdc37dc739022bedb9f3a5bc77c98bdb25bc244
chandrashekar74 <chandrashekarh7409@gamil.com>
2026-02-11 11:19:06 +0530

    completed till conditional

--- Wrapperclass.java
+++ Wrapperclass.java
@@ -12,3 +12,12 @@ System.out.println("The vlue of c wrapperclass is: "+a);
 System.out.println("The vlue of d wrapperclass is: "+a);
     }
  }
+
+ class Wrapperclass2{
+
+    public static void main(String[] args) {
+        String str = "123";
+        int num = Integer.parseInt(str);
+        System.out.println("The vlue of str is: "+str);
+        System.out.println("The vlue of num is: "+num);
+    }
