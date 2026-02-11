ae041f8b7241355fdb032a11244d1d97ed273f3d
chandrashekar74 <chandrashekarh7409@gamil.com>
2026-02-11 10:18:53 +0530

    completed till wrapperclass

---
+++ Wrapperclass.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+ class Wrapperclass{
+
+    public static void main(String[] args) {
+int a = 10;
+Integer b = a;
+Integer c = 10;
+int d=c;
+System.out.println("The vlue of a is: "+a);
+System.out.println("The vlue of b wrapperclass is: "+a);
+System.out.println("The vlue of c wrapperclass is: "+a);
+System.out.println("The vlue of d wrapperclass is: "+a);
+    }
+ }
